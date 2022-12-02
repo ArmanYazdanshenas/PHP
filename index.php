0ac338c5b63b10e70c436550b90ace0bf0872a7c
arman.yazdanshenas <arman.yazdanshenas@gmail.com>
2022-12-02 22:09:27 +0330

    create some php functions

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
+declare(strict_types=1);
 
 echo "Hello PHP!";
+
+function test($count = 7){
+    for ($i=0;$i<$count;$i++){
+        echo "<br />".$i+1;
+    }
+}
+
+function FuncRes($a=0,$b=0):array{
+    $sum = $a + $b;
+    $sub = $a+ - $b;
+    return  [$sum,$sub];
+}
+function RestFunction(...$nums){
+    $sum =0;
+    foreach ($nums as $number){
+        $sum+=$number;
+    }
+    return $sum;
+}
+
+
+test();
+echo "<br />";
+$result = FuncRes(12,9);
+var_dump($result);
+var_dump(count($result));
+
+$my_list = [1,2,3,4,5,6,7,8,9,10];
+echo "<br />";
+var_dump(RestFunction(...$my_list));
+
 
 ?>
 
