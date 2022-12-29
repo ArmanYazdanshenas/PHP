ccc8600918589ef77c47d149bf1064c01f6623a0
arman.yazdanshenas <arman.yazdanshenas@gmail.com>
2022-12-29 22:57:36 +0330

    work on strings

--- index.php
+++ index.php
@@ -1,68 +1,10 @@
 <?php
-declare(strict_types=1);
+$str = 'arman,yazdanshenas,ardeshir,nour';
+$name_array = explode(',',$str);
+var_dump($name_array);
 
-echo "Hello PHP!";
-
-function test($count = 7){
-    for ($i=0;$i<$count;$i++){
-        echo "<br />".$i+1;
-    }
-}
-
-function FuncRes($a=0,$b=0):array{
-    $sum = $a + $b;
-    $sub = $a+ - $b;
-    return  [$sum,$sub];
-}
-function RestFunction(...$nums){
-    $sum =0;
-    foreach ($nums as $number){
-        $sum+=$number;
-    }
-    return $sum;
-}
-$name = 'arman yazdanshenas';
-function PrintName(){
-    global $name;
-    echo $name;
-    $name = 'Arman Yazdanshenas';
-}
-
-function func_test_static()
-{
-    static $age = 0;
-    echo $age;
-    $age++;
-}
-
-$sum = function ($a,$b){
-    return $a+$b;
-};
-//var_dump($sum(4,5));
-
-$c=10;
-$sum = function ($a,$b)use($c){
-    return $a+$b+$c;
-};
-
-$list = [1,2,3,4,5];
-function my_func($num){
-    return $num *2;
-}
-function map($array,$func){
-    $new_list = [];
-    foreach ($array as $key=>$value){
-        $new_list[$key] = $func($value);
-    }
-    return $new_list;
-}
-var_dump(map($list,fn($num)=>$num*3));
-echo ("<br/>");
-$pi = 3.14;
-
-$sum = fn($num1)=>$num1*2;
-var_dump($sum(5));
-
+$new_name_array = implode(',',$name_array);
+var_dump("<br>$new_name_array");
 ?>
 
 <!doctype html>
